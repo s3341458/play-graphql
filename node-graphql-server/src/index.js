8d647e91d9804b6c2c1476e8d0013ee4183e8cc4
Cheng Yu <cheng@commoncode.io>
2019-07-09 17:55:04 +1000

    achieve authentication for graphql

--- node-graphql-server/src/index.js
+++ node-graphql-server/src/index.js
@@ -1,29 +1,22 @@
-const { GraphQLServer } = require('graphql-yoga')
-const { prisma } = require('./generated/prisma-client')
+const { GraphQLServer } = require("graphql-yoga");
+const { prisma } = require("./generated/prisma-client");
+const Query = require("./resolvers/Query");
+const Mutation = require("./resolvers/Mutation");
+const User = require("./resolvers/User");
+const Link = require("./resolvers/Link");
 
 const resolvers = {
-  Query: {
-    info: () => `This is the API of a Hackernews Clone`,
-    feed: (root, args, context, info) => context.prisma.links(),
-  },
-  Mutation: {
-    post: (parent, args, context, info) => {
-      return context.prisma.createLink({
-        url: args.url,
-        description: args.description,
-      })
-    },
-  },
-  Link: {
-    id: (parent) => parent.id,
-    description: (parent) => parent.description,
-    url: (parent) => parent.url,
-  }
-}
+  Query,
+  Mutation,
+  User,
+  Link
+};
 
 const server = new GraphQLServer({
   typeDefs: "./src/schema.graphql",
   resolvers,
-  context: { prisma },
-})
-server.start(() => console.log(`Server is running on http://localhost:4000`))
+  context: request => {
+    return { ...request, prisma };
+  }
+});
+server.start(() => console.log(`Server is running on http://localhost:4000`));
