9a2f5703a504bec9c07fd77b712c3ba2f1a60d2a
Cheng Yu <cheng@commoncode.io>
2019-07-14 23:29:46 +1000

    write a basic react apollo

---
+++ react-apollo/src/components/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import Header from './Header'
+import { Switch, Route } from 'react-router-dom'
+import CreateLink from './CreateLink'
+import LinkList from './LinkList'
+import Login from './Login'
+import '../styles/App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="center w85">
+        <Header />
+        <div className="ph3 pv1 background-gray">
+          <Switch>
+            <Route exact path="/" component={LinkList} />
+            <Route exact path="/create" component={CreateLink} />
+            <Route exact path="/login" component={Login} />
+          </Switch>
+        </div>
+      </div>
+    )
+  }
+
+}
+
+export default App;
---
+++ react-apollo/src/index.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './styles/index.css'
+import App from './components/App'
+import { BrowserRouter } from 'react-router-dom'
+
+// 1
+import { ApolloProvider } from 'react-apollo'
+import { ApolloClient } from 'apollo-client'
+import { createHttpLink } from 'apollo-link-http'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import { AUTH_TOKEN } from './constants'
+import { setContext } from 'apollo-link-context'
+
+
+// 2
+const httpLink = createHttpLink({
+  uri: 'http://localhost:4000'
+})
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem(AUTH_TOKEN)
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : ''
+    }
+  }
+})
+
+// 3
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache()
+})
+
+
+// 4
+ReactDOM.render(
+  <BrowserRouter>
+    <ApolloProvider client={client}>
+      <App />
+  </ApolloProvider>
+  </BrowserRouter>,
+  document.getElementById('root')
+)
